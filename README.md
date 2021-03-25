b005673887c6d74db39ebda5bd8fd35f4c6a7737
Stefan O'Shea <stefan.oshea@tigerspike.com>
2021-03-25 15:55:10 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# shiftman
+# Shiftman
+## _The ultimate shift planner_
+
+Shiftman is an app created as part of the interview process at Deputy.
+
+## Features
+
+- Start and end a shift
+- View history of previous shifts
+- Create a basic user profile
---
+++ shiftman/shiftman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shiftman
+//
+//  Created by Stefan OShea on 25/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
